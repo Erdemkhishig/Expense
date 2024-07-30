86d13847f734b7a011e722a73c49d6bd41ca0054
24LP4153 24LP4153 <24LP4153@G301-CC0195.local>
2024-07-30 12:57:19 +0800

    aaaaaQ

---
+++ Api_frontend/expense-next/src/app/dashboard/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Navbar } from "@/components/Navbar";
+import { Aside } from "@/components/Aside";
+import { Mainheader } from "@/components/Mainheader";
+
+
+export default function dashboard() {
+
+    return (
+        <main>
+            <div className="px-4 max-w-screen-2xl m-auto ">
+                <Navbar />
+                <div className="bg-gray-100 h-full flex py-8 gap-4 px-24">
+                    <Aside />
+                    <div className="ml-8 w-full"><Mainheader /></div>
+                </div>
+
+            </div>
+
+        </main>
+    );
+}
--- Api_frontend/expense-next/src/app/page.js
+++ Api_frontend/expense-next/src/app/page.js
@@ -1,22 +1,17 @@
-import Image from "next/image";
-import { Navbar } from "@/components/Navbar";
-import { Aside } from "@/components/Aside";
-import { Mainheader } from "@/components/Mainheader";
+"use client"
 
+import { useState, useEffect } from 'react'
 
+import { Register } from '../components/Register';
 
 
 export default function Home() {
-  return (
-    <main>
-      <div className="px-4 max-w-screen-2xl m-auto ">
-        <Navbar />
-        <div className="bg-gray-100 h-full flex py-8 gap-4 px-24">
-          <Aside />
-          <div className="ml-8 w-full"><Mainheader /></div>
-        </div>
 
-      </div>
+
+
+  return (
+    <main >
+      <Register />
 
     </main>
   );
