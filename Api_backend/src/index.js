53c36c45be9206474c5af3bf306fdbb069e7cf0f
Erdemkhishig <erdmee1104@gmail.com>
2024-07-30 23:52:43 +0800

    yeah

---
+++ Api_backend/src/index.js
@@ -0,0 +1,52 @@
+
+const express = require("express");
+const cors = require("cors");
+const { accountRouter } = require('./routes/account.route');
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// });
+
+app.use("/accounts", accountRouter);
+
+// app.get("/account", (req,res) => {
+
+// }),
+
+// app.post("/account", (req,res) => {
+
+// }),
+    
+    
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+
+// app.get('/', (req, res) => {
+//     res.json([
+//         {
+//             title: "Hello World 1",
+//         },
+//         {
+//             title: "Hello World 2",
+//         },
+//         {
+//             title: "Hello World 3",
+//         },
+//         {
+//             title: "Hello World 4",
+//         },
+//         {
+//             title: "Hello World 5",
+//         },
+//     ])
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
--- Api_frontend/expense-next/src/app/page.js
+++ Api_frontend/expense-next/src/app/page.js
@@ -3,15 +3,68 @@
 import { useState, useEffect } from 'react'
 
 import { Register } from '../components/Register';
+import axios from 'axios';
 
 
 export default function Home() {
+  const [accounts, setAccounts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
 
 
+useEffect(() => {
+ const getData = async () => {
+  const response = await axios.get("http://localhost:3001/accounts");
+  setAccounts(response.data);
+  
+ };
+ getData()
+
+}, []);
+
+const createAccount = async () => {
+  const newAccount = {
+    title,
+    amount,
+  };
+
+  const response = await axios.post("http://localhost:3001/accounts",
+  newAccount
+);
+setAccounts([...accounts, response.data]);
+};
 
   return (
     <main >
+      
+      <div>
+        <p>Account</p>
+        {accounts.map((account, index) => (
+          <li key={account.title + index}>
+            {account.title} - {account.amount}
+            </li>
+        ))}
+
+        <div className='flex gap-4 p-6'>
+          <input className='border'
+          value={title}
+          onChange={(event) =>{
+            setTitle(event.target.value);
+          }}/>
+          
+          <input className='border'
+          value={amount}
+          onChange={(event) =>{
+            setAmount(event.target.value);
+          }}/>
+
+        </div>
+
+        <button onClick={createAccount}>create</button>
+
+      </div>
       <Register />
+      
 
     </main>
   );
